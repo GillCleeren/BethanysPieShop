783ff09b1d5ed7df038e457cba251cfc9ce9ef17
Bozhi Qian <bozhiqian@gmail.com>
2017-12-21 22:07:46 +1100

    Fixed the issue occurred when running update-database in the package maanager: Unable to create an object of type 'AppDbContext'. Add an implementation of 'IDesignTimeDbContextFactory' to the project,
    And appended some missing change needed when upgrading from core 1.* to core 2.0. Thanks Gill for your a lot of excellent courses on Pluralsight that I learned.

--- src/BethanysPieShop/Models/DbInitializer.cs
+++ src/BethanysPieShop/Models/DbInitializer.cs
@@ -1,26 +1,29 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace BethanysPieShop.Models
 {
-    public static class DbInitializer
+    public class DbInitializer
     {
-        public static void Seed(IApplicationBuilder applicationBuilder)
-        {
-            AppDbContext context = applicationBuilder.ApplicationServices.GetRequiredService<AppDbContext>();
+        private readonly AppDbContext _context;
 
-            if (!context.Categories.Any())
+        public DbInitializer(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+        }
+        public void Seed()
+        {
+            _context.Database.EnsureCreated();
+
+
+            if (!_context.Categories.Any())
             {
-                context.Categories.AddRange(Categories.Select(c => c.Value));
+                _context.Categories.AddRange(Categories.Select(c => c.Value));
             }
 
-            if (!context.Pies.Any())
+            if (!_context.Pies.Any())
             {
-                context.AddRange
+                _context.AddRange
                 (
                     new Pie { Name = "Apple Pie", Price = 12.95M, ShortDescription = "Our famous apple pies!", LongDescription = "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.", Category = Categories["Fruit pies"], ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/applepie.jpg", InStock = true, IsPieOfTheWeek = true, ImageThumbnailUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/applepiesmall.jpg", AllergyInformation = "" },
                     new Pie { Name = "Blueberry Cheese Cake", Price = 18.95M, ShortDescription = "You'll love it!", LongDescription = "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.", Category = Categories["Cheese cakes"], ImageUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/blueberrycheesecake.jpg", InStock = true, IsPieOfTheWeek = false, ImageThumbnailUrl = "https://gillcleerenpluralsight.blob.core.windows.net/files/blueberrycheesecakesmall.jpg", AllergyInformation = "" },
@@ -36,7 +39,7 @@ namespace BethanysPieShop.Models
                 );
             }
 
-            context.SaveChanges();
+            _context.SaveChanges();
         }
 
         private static Dictionary<string, Category> categories;
